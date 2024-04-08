d4e3802c908916a3eaf6b95f042f767516a65b44
AndriKovalchuk <andrii.kovalchuk90@gmail.com>
2024-04-08 14:56:48 +0300

    first_commit

---
+++ website_project/users/forms.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import CharField, TextInput, EmailInput, EmailField, PasswordInput
+from django import forms
+
+from .models import Profile
+
+
+class RegistrationForm(UserCreationForm):
+    username = CharField(max_length=16, min_length=3, required=True,
+                         widget=TextInput(attrs={"class": "form-control"}))
+
+    email = EmailField(max_length=25, required=True,
+                       widget=EmailInput(attrs={"class": "form-control"}))
+
+    password1 = CharField(required=True,
+                          widget=PasswordInput(attrs={"class": "form-control"}))
+
+    password2 = CharField(required=True,
+                          widget=PasswordInput(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class ProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput())
+
+    class Meta:
+        model = Profile
+        fields = ['avatar']
---
+++ website_project/users/views.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import logout
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.contrib import messages
+from django.views.generic import DetailView, TemplateView
+from django.contrib.auth.views import PasswordResetView
+
+from .forms import RegistrationForm
+from .models import Profile
+
+
+class RegisterView(View):
+    template_name = "users/register.html"
+    form_class = RegistrationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(to="quotes:root")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        return render(request, self.template_name, context={"form": self.form_class})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            messages.success(request, f"Registration of {username} has been successfully completed.")
+            return redirect(to="users:signin")
+        return render(request, self.template_name, context={"form": form})
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'profile.html'
+    context_object_name = 'user_profile'
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
+
+
+class UserLogoutView(View):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect(reverse_lazy("users:logout_page"))
+
+
+class UserLogoutPageView(TemplateView):
+    template_name = 'users/logout.html'
